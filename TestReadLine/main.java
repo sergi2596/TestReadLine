a068d6dce5bb8ae26959342c480e03b045223323
Sergi Sanchez Deutsch <s.s.deutsch72@gmail.com>
2017-10-07 12:22:41 +0200

    change deleted

--- TestReadLine/main.java
+++ TestReadLine/main.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
 
-//change added to master branch in github
-
 class TestReadLine {
     public static void main(String[] args) {
         EditableBufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
